22705cb34fe30438b2b9ef38c701fc0cf1e75e6c
nsedan <naim.alejandro.sedan@gmail.com>
2021-07-22 16:36:48 +0100

    react icons added

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { todosActions, TodoType } from "../store/todo-slice";
 import classes from "./TodoItem.module.css";
+import {
+  FaExclamationTriangle,
+  FaMinusCircle,
+  FaCheckCircle,
+  FaTimesCircle,
+  FaTrash,
+} from "react-icons/fa";
 
 const TodoItem = (props: TodoType) => {
   const { title, highPriority, isCompleted, id } = props;
@@ -23,9 +30,23 @@ const TodoItem = (props: TodoType) => {
     <li className={classes.listItem}>
       <h3 className={classes.itemTitle}>{title}</h3>
       <div className={classes.itemActions}>
-        <button onClick={onTogglePriority}>{highPriority ? "!" : "-"}</button>
-        <button onClick={onToggleCompleted}>{isCompleted ? "✓" : "x"}</button>
-        <button onClick={onDeleteHandler}>x</button>
+        <button onClick={onTogglePriority}>
+          {highPriority ? (
+            <FaExclamationTriangle size="15" color={"red"} />
+          ) : (
+            <FaMinusCircle size="15" />
+          )}
+        </button>
+        <button onClick={onToggleCompleted}>
+          {isCompleted ? (
+            <FaCheckCircle size="15" color={"green"} />
+          ) : (
+            <FaTimesCircle size="15" />
+          )}
+        </button>
+        <button onClick={onDeleteHandler}>
+          <FaTrash size="15" />
+        </button>
       </div>
     </li>
   );
